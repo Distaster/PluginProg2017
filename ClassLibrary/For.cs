40ff92eccf93b66d86215346cd79b182d113b669
Unknown <philipp.zuelicke@gmx.de>
2017-05-25 15:58:57 +0200

    MEF Einrichtung
    MEF Einrichtung in einem anderen Projekt

---
+++ ClassLibrary/For.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    [Export(typeof(LoopInterface))]
+    class For : LoopInterface
+    {
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ PluginProg/CodeGeneratorControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CodeGeneratorControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace PluginProg
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for CodeGeneratorControl.
+    /// </summary>
+    public partial class CodeGeneratorControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeGeneratorControl"/> class.
+        /// </summary>
+        public CodeGeneratorControl()
+        {
+            this.InitializeComponent();
+            Collection col = new Collection();
+            listi.ItemsSource = col.getLoops();
+            listi2.ItemsSource = col.getConditions();
+        }
+
+        
+    }
+}
